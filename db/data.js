6b1e9f01d1360f5742d2eb0bd3c3b0566f3ae58a
gitvalentina <vdouce@udesa.edu.ar>
2022-04-17 17:26:36 -0300

    iterar productos novedades

--- db/data.js
+++ db/data.js
@@ -7,16 +7,16 @@ const computadoras = {
         contra: "4871Leyla_"},
 
     products: [
-        {id: "1", title: 'Acer Aspire 5', photo: '/images/produts/AcerAspire5.jpg'},
-        {id: "2", title: 'Alienware 15', photo: '/images/products/Alienware15.jpg'},
-        {id: "3", title: 'Asus ROG Strix', photo: '/images/products/AsusROGStrix.jpg'},
-        {id: "4", title: 'Dell Inspiron', photo: '/images/products/DellInspiron.jpg'},
-        {id: "5", title: 'HP 15', photo: '/images/products/HP15.jpg'},
-        {id: "6", title: 'HP Pavillion', photo: '/images/products/HpPavillion.jpg'},
-        {id: "7",title: 'Mackboock Pro', photo: '/images/products/macbookpro.jpg'},
-        {id: "8", title: 'Razer Blade 15', photo: '/images/products/RazerBlade.jpg'},
-        {id: "9", title: 'Xaomi Mi Pro', photo: '/images/products/XaomiMiPro.jpg'},
-        {id: "10", title: 'Microsoft Surface Pro', photo: '/images/products/Microsoft.jpg'},
+        {id: "1", title: 'Acer Aspire 5', description: 'Procesador intel core: i5', photo: '/images/produts/AcerAspire5.jpg'},
+        {id: "2", title: 'Alienware 15', description: 'Procesador intel core: i5',photo: '/images/products/Alienware15.jpg'},
+        {id: "3", title: 'Asus ROG Strix', description: 'Procesador intel core: i5', photo: '/images/products/AsusROGStrix.jpg'},
+        {id: "4", title: 'Dell Inspiron', description: 'Procesador intel core: i5', photo: '/images/products/DellInspiron.jpg'},
+        {id: "5", title: 'HP 15', description: 'Procesador intel core: i5', photo: '/images/products/HP15.jpg'},
+        {id: "6", title: 'HP Pavillion', description: 'Procesador intel core: i5', photo: '/images/products/HpPavillion.jpg'},
+        {id: "7",title: 'Mackboock Pro', description: 'Procesador intel core: i5', photo: '/images/products/macbookpro.jpg'},
+        {id: "8", title: 'Razer Blade 15', description: 'Procesador intel core: i5', photo: '/images/products/RazerBlade.jpg'},
+        {id: "9", title: 'Xaomi Mi Pro', description: 'Procesador intel core: i5', photo: '/images/products/XaomiMiPro.jpg'},
+        {id: "10", title: 'Microsoft Surface Pro', description: 'Procesador intel core: i5', photo: '/images/products/Microsoft.jpg'},
     ],
 
     comments: [
