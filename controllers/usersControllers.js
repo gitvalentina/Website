8e6cf8d08cd71dab5e5f6d0c75e7dbab28729528
gitvalentina <vdouce@udesa.edu.ar>
2022-04-16 21:39:54 -0300

    correccion

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,13 +4,13 @@ const controlador={
     login: function(req, res, next) {
         res.render('login', { title: 'Express' });
       },
-    profile: function name(req, res, next) {
+    profile: function (req, res, next) {
             res.render('profile', {title: 'Express'});
     },
     profileEdit: function(req, res, next) {
         res.render('profile-edit', { title: 'Express' });
       },
-    register: function name(req, res, next) {
+    register: function (req, res, next) {
             res.render('register', {title: 'Express'});
     },
 }
